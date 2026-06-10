b38d922edafb2f4f3bc0889b07253de9d8998e62
2001aswin89 <2001aswin89@gmail.com>
2026-06-10 20:49:36 +0530

    feat: configure mongodb atlas and mongoose

---
+++ src/schemas/ParkingSpotSchema.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+import { SpotType } from "../enums/SpotType";
+
+const ParkingSpotSchema = new Schema(
+    {
+        spotNumber: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+
+        floorNumber: {
+            type: Number,
+            required: true,
+        },
+
+        type: {
+            type: String,
+            enum: Object.values(SpotType),
+            required: true,
+        },
+
+        occupied: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+export const ParkingSpotModel =
+    mongoose.model(
+        "ParkingSpot",
+        ParkingSpotSchema,
+    );
